67fa04453a98987340c9493abd551b98bab6ad20
MohamadReza Deylami <hello@imohamad.me>
2016-04-26 23:58:42 +0430

    Upload Files

---
+++ js/app.js
@@ -0,0 +1,68 @@
+var movieList = angular.module('movieList', [
+  'ngRoute',
+  'moviesController'
+]);
+
+var moviesController = angular.module('moviesController', []);
+
+//config app & pages
+movieList.config(['$routeProvider', function($routeProvider){$routeProvider.
+
+    when('/movies', {
+      templateUrl: 'pages/movie-list.html',
+      controller: 'ListMovie'
+
+    }).when('/detail/:movieId', { 
+      templateUrl: 'pages/detail.html',
+      controller: 'DetailMovie'  
+
+    }).otherwise({
+      redirectTo: '/movies'
+    });
+
+}]);
+
+//list controller
+moviesController.controller('ListMovie', ['$scope', '$http', function ($scope, $http){
+
+  $scope.loading = true; //for show loading
+  $http.get('https://dl.dropboxusercontent.com/s/eunjr4op0aj3kia/movies.json').success(function(data){
+
+  $scope.movies = data; // insert json data to movies
+  $scope.loading = false; // for hide loading
+
+  }); 
+
+}]);
+
+//detail controller
+moviesController.controller('DetailMovie', [
+  '$scope', '$http', '$routeParams', function ($scope, $http, $routeParams){
+
+  $scope.loading = true; //for show loading
+  $http.get('https://dl.dropboxusercontent.com/s/eunjr4op0aj3kia/movies.json').success(function(data){
+
+  $scope.movies = data; // insert json data to movies
+  $scope.movieId = $routeParams.movieId; //set id to movieId
+
+
+  // for nex & prev page
+  if($routeParams.movieId > 0){
+    $scope.prevMovie = Number($routeParams.movieId) - 1;
+  } else{
+    $scope.prevMovie = $scope.movies.length - 1;
+  }
+
+  if($routeParams.movieId < $scope.movies.length - 1){
+    $scope.nextMovie = Number($routeParams.movieId) + 1;
+  } else{
+    $scope.nextMovie = 0;
+  }
+
+  $scope.loading = false; //for hide loading
+
+  });
+  
+}]);
+
+
